0bd645fd1cebf2487cd97675a243294f2211a0b6
katimoto <kati.miyamoto@gmail.com>
2020-10-08 19:46:35 +0900

    記事、質問、チャット機能完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users, controllers: {
+   omniauth_callbacks: 'users/omniauth_callbacks',
+   registrations: 'users/registrations'
+ }
   get 'messages/index'
-  root to: "rooms#index"
-  resources :users, only: [:edit, :update]
+  root to: 'articles#index'
+  # root to: "rooms#index"
+  resources :users, only: [:edit, :update, :show]
+  resources :articles do
+    resources :favorites, only: :create
+    collection do
+      get 'search'
+    end
+  end
+  resources :questions do
+  resources :answers, only: :create
+    collection do
+      get 'search'
+    end
+  end
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
