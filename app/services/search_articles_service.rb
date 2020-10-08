4980e955144085fddb87d6077454a272eeaa75c4
katimoto <kati.miyamoto@gmail.com>
2020-10-08 19:46:17 +0900

    検索機能

---
+++ app/services/search_articles_service.rb
@@ -0,0 +1,9 @@
+class SearchArticlesService
+  def self.search(search)
+    if search != ""
+      Article.where('text LIKE(?)', "%#{search}%")
+    else
+      Article.all
+    end
+  end
+end
---
+++ app/services/search_questions_service.rb
@@ -0,0 +1,9 @@
+class SearchQuestionsService
+  def self.search(search)
+    if search != ""
+      Question.where('text LIKE(?)', "%#{search}%")
+    else
+      Question.all
+    end
+  end
+end
