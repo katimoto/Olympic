6b102d2c96fcc31a79e6b12212b21f63411e5df7
katimoto <kati.miyamoto@gmail.com>
2020-10-08 19:39:16 +0900

    記事投稿機能

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,9 @@
+module ArticlesHelper
+  def article_lists(articles)
+    html = ''
+    articles.each do |article|
+      html += render(partial: 'article',locals: { article: article })
+    end
+    return raw(html)
+  end
+end
--- db/migrate/20201002093744_create_articles.rb
+++ db/migrate/20201002093744_create_articles.rb
@@ -1,7 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-
+      t.string :name, null: false
+      t.string :text, null: false
+      t.text :image
       t.timestamps
     end
   end
