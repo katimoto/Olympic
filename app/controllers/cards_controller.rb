103b9d8c7250e4367d2ede0f36eeb08adea30c5d
katimoto <kati.miyamoto@gmail.com>
2020-10-14 23:53:51 +0900

    各indexの変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -20,4 +20,13 @@ class CardsController < ApplicationController
       redirect_to "new" # カード登録画面
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+
+    customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
+    @card = customer.cards.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,5 @@ Rails.application.routes.draw do
     get 'order_new', on: :member
     resources :messages, only: [:index, :create]
   end
-  resources :cards, only: [:new, :create]
+  resources :cards, only: [:new, :create, :show]
 end
