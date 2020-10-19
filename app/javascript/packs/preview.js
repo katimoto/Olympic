4ef724fd38f1c9b2cc6eb3be69b248790c26ba64
katimoto <kati.miyamoto@gmail.com>
2020-10-19 19:04:47 +0900

    複数プレビューのサーバー完成

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,31 +1,43 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('image-list');
+  document.addEventListener('DOMContentLoaded', function() {
+    const ImageList = document.getElementById('image-list')
 
+    // 選択した画像を表示する関数
     const createImageHTML = (blob) => {
-       // 画像を表示するためのdiv要素を生成
-      const imageElement = document.createElement('div');
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
       // 表示する画像を生成
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
+
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `articles_tag_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'articles_tag[images][]')
+      inputHTML.setAttribute('type', 'file')
 
       // 生成したHTMLの要素をブラウザに表示させる
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    };
-    
-    document.getElementById('articles_tag_image').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
 
-      createImageHTML(blob);
+        createImageHTML(blob)
+      })
+    }
+
+    document.getElementById('articles_tag_image').addEventListener('change', (e) => {
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+
     });
   });
 }
