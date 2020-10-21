34ab570cd8320187a64f4420259d86a37f7ef09e
katimoto <kati.miyamoto@gmail.com>
2020-10-21 12:45:53 +0900

    アンサーデバッグ

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,68 +1,22 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
-  @@question_id = nil
-
-  def index
-    @questions = Question.all
-  end
- 
-  def show
-  end
- 
-  def new
-    @question = Question.find(params[:id])
-    @answer = Answer.new
-  end
- 
-  def edit
-  end
- 
   def create
-    @answer = Answer.new(answer_params)
-    # question_idを代入
-    @answer.question_id = @@question_id
-    # user_idを代入
+    @question = Question.find(params[:question_id])
+    #投稿に紐づいたコメントを作成
+    @answer = @question.answers.build(answer_params)
     @answer.user_id = current_user.id
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render 'views/question/show', status: :created, location: @answer }
-      else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
+    @answer.save
+    redirect_to question_path(@question.id)
   end
- 
-  def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
- 
+
   def destroy
-    @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
- 
-  private
-
-  def set_answer
     @answer = Answer.find(params[:id])
+    @answer.destroy
+    render :index
   end
 
+  private
   def answer_params
-    params.require(:answer).permit(:text).merge(user_id: current_user.id, question_id: params[:question_id])
+    params.permit(:question_id).merge(user_id: current_user.id, text: params[:answer][:text])
   end
 end
 
