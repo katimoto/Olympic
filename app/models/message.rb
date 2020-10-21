f3b9e3cb2efef7d1037130a29d2b5e3b23139793
katimoto <kati.miyamoto@gmail.com>
2020-10-21 09:42:04 +0900

    devise日本語化

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,9 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
-  validates :content, presence: true, unless: :was_attached?
+  
+  validates :content, presence: true, length: { maximum: 500 }, unless: :was_attached?
+  validates :user_id, presence: true
 
   def was_attached?
     self.image.attached?
