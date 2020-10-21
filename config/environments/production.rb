9ddc40d3cd8f59091ccb7a7e8adf281f3565527b
katimoto <kati.miyamoto@gmail.com>
2020-10-21 09:51:02 +0900

    Action Cableの本番構築

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+ActionCable.server.config.disable_request_forgery_protection = true
+config.action_cable.url = 'ws://54.199.73.178//cable'
+config.action_cable.allowed_request_origins = [ 'http://54.199.73.178' ]
 end
