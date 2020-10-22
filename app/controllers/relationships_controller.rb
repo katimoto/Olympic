f99269f3f953be8954278be28b8616a204ddd4ad
katimoto <kati.miyamoto@gmail.com>
2020-10-23 01:25:42 +0900

    フォローの非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,27 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to users_path
+  
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to users_path
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
