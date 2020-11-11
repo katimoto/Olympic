c27061c01073985911cb9a4f0d635ac1b14bb02e
katimoto <kati.miyamoto@gmail.com>
2020-11-11 23:14:33 +0900

    cssの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   helper_method :user_info
 
   def user_info(user_id)
